86ed6def261acb9334543612bca12101d2775759
24-Muhammad-Ikhsan-X-PPLG-2 <ikhsanm181209@gmail.com>
2026-06-20 13:29:28 +0700

    feat: landing page

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,9 +1,13 @@
 import { createInertiaApp } from '@inertiajs/react';
 import { configureEcho } from '@laravel/echo-react';
+import { Container, createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 configureEcho({
     broadcaster: 'reverb',
 });
+
+const queryClient = new QueryClient();
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -11,5 +15,12 @@ createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
     progress: {
         color: '#4B5563',
+    },
+    setup({ App, el, props }) {
+        createRoot(el as Container).render(
+            <QueryClientProvider client={queryClient}>
+                <App {...props} />
+            </QueryClientProvider>,
+        );
     },
 });
